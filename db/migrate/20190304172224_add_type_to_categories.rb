42cca4f32a95154ba7c6611b071ed85ff0bd8cf7
Jenny Wagner <jennywagg@gmail.com>
2019-03-07 11:43:28 -0500

    Take care of 'type' inheritance column problem

--- db/migrate/20190304172224_add_type_to_categories.rb
+++ db/migrate/20190304172224_add_type_to_categories.rb
@@ -1,7 +1,10 @@
 class AddTypeToCategories < ActiveRecord::Migration[5.1]
-  def change
+  def up
     add_column :categories, :type, :string
     Category.update_all( {type: 'service'} )
     change_column :categories, :type, :string, null: false
+  end
+  def down
+    remove_column :categories, :type, :string
   end
 end
