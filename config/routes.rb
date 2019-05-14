c3a6ba00bb1efb1c69fd1d02da7c923b7f4f726f
Doug Price <dprice@fearless.tech>
2019-05-14 17:00:28 -0400

    AHC-1366: updates style of the admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   constraints(SubdomainConstraints.new(subdomain: ENV['ADMIN_SUBDOMAIN'])) do
     namespace :admin, path: ENV['ADMIN_PATH'] do
       root to: 'dashboard#index', as: :dashboard
+      get '/cvs_downloads', to: 'dashboard#cvs_downloads', as: "cvs_downloads"
 
       resources :locations, except: :show do
         resources :services, except: %i[show index] do
