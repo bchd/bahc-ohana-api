3e82da5f3cac5744a3993a38168ec7487084708d
Justus Eapen <justuseapen@gmail.com>
2020-04-10 01:45:05 -0400

    Fixes chewy init

--- config/initializers/chewy.rb
+++ config/initializers/chewy.rb
@@ -1,12 +1,3 @@
 # frozen_string_literal: true
 
-elasticsearch_domain = ENV.fetch(ENV.fetch('ELASTICSEARCH_DOMAIN_PROVIDER'))
-elasticsearch_port = ENV.fetch(ENV.fetch('ELASTICSEARCH_PORT_PROVIDER'))
-elasticsearch_url = "#{elasticsearch_domain}:#{elasticsearch_port}"
-
-chewy_config = {
-  host: elasticsearch_url,
-  logger: Rails.logger
-}
-
-Chewy.settings = chewy_config
+Chewy.settings = { host: ENV['SEARCHBOX_URL'] || 'localhost:9200' }
