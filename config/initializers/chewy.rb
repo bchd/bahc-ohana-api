23065abdb46e31dbd07b6ac10d6a8002a4052670
Santosh Wadghule <santosh.wadghule@gmail.com>
2020-04-15 21:45:23 +0530

    Set specs and Elasticsearch on Drone CI

--- config/initializers/chewy.rb
+++ config/initializers/chewy.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
+local_ealasticsearch_url = 'localhost:9200'
+
 Chewy.settings = if Rails.env.test?
                    {
-                     host: 'localhost:9200',
+                     host: ENV['ELASTICSEARCH_URL'] || local_ealasticsearch_url,
                      prefix: 'test'
                    }
                  else
                    {
-                     host: ENV['BONSAI_URL'] || 'localhost:9200',
+                     host: ENV['BONSAI_URL'] || local_ealasticsearch_url,
                      request_timeout: 240
                    }
                  end
