ba267bd96c11ece654fff1acff6590b0fb8c91c6
Santosh Wadghule <santosh.wadghule@gmail.com>
2020-06-12 22:13:21 +0530

    Handle report attribute label

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -11,6 +11,44 @@ class Flag < ApplicationRecord
 
   validate :check_report_attributes
 
+  # Keep this method consistent with UI app too (in Flag model)
+  def self.report_attributes_schema
+    [
+      {
+        name: :hours_location_contact_info_incorrect,
+        label: "Hours, Location, Contact info incorrect"
+      },
+      {
+        name: :the_service_listed_are_incorrect,
+        label: "The services listed are incorrect"
+      },
+      {
+        name: :the_eligibility_how_to_access_or_waht_to_bring_is_incorrect,
+        label: "The eligibility, how to access, or what to bring is incorrect"
+      },
+      {
+        name: :other,
+        label: "Other"
+      },
+      {
+        name: :employee_of_the_org,
+        label: "I am employee of the org"
+      }
+    ]
+  end
+
+  def self.get_report_attribute_label_for(attr)
+    attribute = report_attributes_schema.find do |ar|
+      ar[:name].to_s == attr[0]
+    end
+
+    if found_attribute
+      found_attribute[:label]
+    else
+      attr[0]
+    end
+  end
+
   def resource_path
     paths = Rails.application.routes.url_helpers
 
