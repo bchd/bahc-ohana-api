ca71fbe6ff42b694c6660bad5fffa36731945679
Santosh Wadghule <santosh.wadghule@gmail.com>
2020-06-12 18:07:51 +0530

    More specs

--- spec/models/flag_spec.rb
+++ spec/models/flag_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe Flag, type: :model do
   subject { Flag.new }
 
+  it { is_expected.to belong_to(:resource) }
+
   context "validations" do
     it do
       is_expected.to validate_presence_of(:report_attributes).
@@ -20,6 +22,12 @@ describe Flag, type: :model do
       subject.valid?
 
       expect(subject.errors[:report_attributes].first).to be_eql("can't be blank")
+    end
+
+    it "should validate wrong email address" do
+      subject.email = "abc"
+      subject.valid?
+      expect(subject.errors[:email].first).to be_eql("wrong format")
     end
   end
 end
