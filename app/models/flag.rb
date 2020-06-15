7c806d597b70d8911914240417f740bb1399797e
Santosh Wadghule <santosh.wadghule@gmail.com>
2020-06-15 22:06:15 +0530

    Remove report_attributes_schema.

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -11,44 +11,6 @@ class Flag < ApplicationRecord
 
   validate :check_report_attributes
 
-  # Keep this method consistent with UI app too (in Flag model)
-  def self.report_attributes_schema
-    [
-      {
-        name: :hours_location_contact_info_incorrect,
-        label: "Hours, Location, Contact info incorrect"
-      },
-      {
-        name: :the_service_listed_are_incorrect,
-        label: "The services listed are incorrect"
-      },
-      {
-        name: :the_eligibility_how_to_access_or_waht_to_bring_is_incorrect,
-        label: "The eligibility, how to access, or what to bring is incorrect"
-      },
-      {
-        name: :other,
-        label: "Other"
-      },
-      {
-        name: :employee_of_the_org,
-        label: "I am employee of the org"
-      }
-    ]
-  end
-
-  def self.report_attributes_in_order
-    Flag.report_attributes_schema.collect(&:values).transpose[0]
-  end
-
-  def self.get_report_attribute_label_for(attr)
-    attribute = report_attributes_schema.find do |ar|
-      ar[:name] == attr
-    end
-
-    attribute ? attribute[:label] : attr[0]
-  end
-
   def resource_path
     paths = Rails.application.routes.url_helpers
 
@@ -65,7 +27,7 @@ class Flag < ApplicationRecord
   private
 
   def check_report_attributes
-    if report_attributes.nil? || report_attributes.all? {|k, v| v.blank? || v == "0" }
+    if report_attributes.nil? || report_attributes.all? {|k, v| v["value"].blank? || v["value"] == "0" }
       errors.add(:report_attributes, "can't be blank")
     end
   end
