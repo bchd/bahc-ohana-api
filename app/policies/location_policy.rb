9a1e267e3398f5e1ca8fe424bd21b034e16ae812
Melvin Cedeno <cedenomelvin@gmail.com>
2020-06-18 19:21:37 -0600

    adds archive flag to services

--- app/policies/location_policy.rb
+++ app/policies/location_policy.rb
@@ -8,10 +8,9 @@ class LocationPolicy < ApplicationPolicy
 
   class Scope < Scope
     def resolve
-
       return scope.pluck(:id, :name, :slug, :archived).sort_by(&:second) if user.super_admin?
 
-      scope.with_email(user.email).pluck(:id, :name, :slug, :archived)
+      scope.with_email(user.email).pluck(:id, :name, :slug)
     end
   end
 end
