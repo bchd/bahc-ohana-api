ffcdbb70f1ec400e95cbfae2bb2b90c74656eb60
Melvin Cedeno <cedenomelvin@gmail.com>
2020-06-30 10:15:43 -0600

    adds default nil to locations archived at migration

--- db/migrate/20200614183600_add_achive_status_to_locations.rb
+++ db/migrate/20200614183600_add_achive_status_to_locations.rb
@@ -1,5 +1,5 @@
 class AddAchiveStatusToLocations < ActiveRecord::Migration[5.1]
   def change
-    add_column :locations, :archived_at, :datetime
+    add_column :locations, :archived_at, :datetime, default: nil
   end
 end
