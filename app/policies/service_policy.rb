8b369eb3a4da5ad838bd68a878ff8bb8d1892367
Stephanie Vizzi <stephanie@smartlogic.io>
2020-06-30 21:09:40 -0400

    rm conflict syntax

--- app/policies/service_policy.rb
+++ app/policies/service_policy.rb
@@ -1,11 +1,7 @@
 class ServicePolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-<<<<<<< HEAD
       return scope.order(:name) if user.super_admin?
-=======
-      return scope.pluck(:location_id, :id, :name, :archived_at).sort_by(&:third) if user.super_admin?
->>>>>>> 35dc1ce78e989761bc42f9a793edb3bece452cdc
 
       scope.with_locations(location_ids).order(:name)
     end
