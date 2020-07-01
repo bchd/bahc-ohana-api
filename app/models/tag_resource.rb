fd8bbb7046f2a6a59e40c13824e9e75aaff8785c
Stephanie Vizzi <stephanie@smartlogic.io>
2020-07-01 15:16:47 -0400

    Remove function used before refactor

--- app/models/tag_resource.rb
+++ app/models/tag_resource.rb
@@ -1,9 +1,4 @@
 class TagResource < ApplicationRecord
   belongs_to :tag
   belongs_to :resource, polymorphic: true
-
-  def self.get_resources (tag_id)
-    query = where("tag_id = ?", tag_id).select(:resource_id, :resource_type)
-    return query.pluck(:id, :resource_id, :resource_type)
-  end
 end
