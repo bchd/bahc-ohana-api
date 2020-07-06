a1e15235727375fcdea4527f0930ed0a7dc8a221
Melvin Cedeno <cedenomelvin@gmail.com>
2020-07-06 13:51:47 -0600

    removes default heap line from sql

--- db/structure.sql
+++ db/structure.sql
@@ -658,7 +658,8 @@ CREATE TABLE public.services (
     interpretation_services text,
     wait_time_updated_at timestamp without time zone,
     icarol_categories character varying,
-    archived_at timestamp without time zone
+    archived_at timestamp without time zone,
+    address_details text
 );
 
 
@@ -1457,7 +1458,7 @@ CREATE UNIQUE INDEX unique_schema_migrations ON public.schema_migrations USING b
 -- Name: locations locations_search_content_trigger; Type: TRIGGER; Schema: public; Owner: -
 --
 
-CREATE TRIGGER locations_search_content_trigger BEFORE INSERT OR UPDATE ON public.locations FOR EACH ROW EXECUTE PROCEDURE public.fill_search_vector_for_location();
+CREATE TRIGGER locations_search_content_trigger BEFORE INSERT OR UPDATE ON public.locations FOR EACH ROW EXECUTE FUNCTION public.fill_search_vector_for_location();
 
 
 --
