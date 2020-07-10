08952026070c2e96ffbfc6ae192345a3fdf65e5f
Stephanie Vizzi <stephanie@smartlogic.io>
2020-07-10 15:50:02 -0400

    Batch Archive tests: - controller test: archive a single location - feature test: 2 separate features for testing appearance of archive capabilities based on roles

--- spec/features/admin/services/batch_archive_spec.rb
+++ spec/features/admin/services/batch_archive_spec.rb
@@ -9,11 +9,29 @@ feature 'Archive a service' do
     visit '/admin/services'
   end
 
+  scenario 'accessible to super admin' do
+    assert page.has_css?('.archive-btn')
+  end
+  
   scenario 'by select all', js: true do
     check "Select all on page"
 
     click_button I18n.t('admin.buttons.batch_archive_services')
 
     expect(page).to have_content 'Archive successful.'
+  end
+end
+
+feature 'Archiving services' do
+  background do
+    @location = create(:location)
+    @service = @location.services.create!(attributes_for(:service).merge!(name: 'Book Bus'))
+    @service = @location.services.create!(attributes_for(:service).merge!(name: 'Vocational Service'))
+    login_admin
+    visit '/admin/services'
+  end
+
+  scenario 'not accessible to regular admin' do
+    assert page.has_no_css?('.archive-btn')
   end
 end
