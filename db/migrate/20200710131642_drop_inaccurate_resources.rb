1c5742c699f1f95f2f3452978a2fa1dd5011d940
Tina G <tina@smartlogic.io>
2020-07-13 10:06:04 -0400

    Drone: Chnage node version to 12

--- db/migrate/20200710131642_drop_inaccurate_resources.rb
+++
@@ -1,9 +0,0 @@
-class DropInaccurateResources < ActiveRecord::Migration[5.2]
-  def up
-    drop_table :inaccurate_resources
-  end
-
-  def down
-    fail ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20200710131654_drop_inaccurate_resource_categories.rb
+++
@@ -1,9 +0,0 @@
-class DropInaccurateResourceCategories < ActiveRecord::Migration[5.2]
-  def up
-    drop_table :inaccurate_resource_categories
-  end
-
-  def down
-    fail ActiveRecord::IrreversibleMigration
-  end
-end
