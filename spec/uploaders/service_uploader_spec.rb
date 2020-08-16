df3ef7d3d747328f6f4ba490e3be0c374aad7ef3
Stephanie Vizzi <stephanie@smartlogic.io>
2020-08-16 19:12:29 -0400

    final unit tests

--- spec/uploaders/service_uploader_spec.rb
+++ spec/uploaders/service_uploader_spec.rb
@@ -29,13 +29,34 @@ describe ServiceUploader do
       end
 
       it 'should error if the location is not found' do
-        location_not_found_csv = Rails.root.join("spec", "support", "fixtures", "service_upload_location_not_found.csv")
+        location_not_found_csv = Rails.root.join(
+          "spec",
+          "support",
+          "fixtures",
+          "service_upload_location_not_found.csv"
+        )
         uploader = ServiceUploader.new(location_not_found_csv)
         
-        expect { uploader.process }.to raise_error(ServiceUploader::ServiceUploadError, 'Upload unsuccessful. Location with id: -1 not found')
+        expect { uploader.process }.to raise_error(
+          ServiceUploader::ServiceUploadError,
+          'Upload unsuccessful. Location with id: -1 not found'
+        )
       end
 
-      # it 'should error if the service has a validation error'
+      it 'should error if the service has a validation error' do
+        invalid_url_csv = Rails.root.join(
+          "spec",
+          "support",
+          "fixtures",
+          "service_upload_invalid_url.csv"
+        )
+        uploader = ServiceUploader.new(invalid_url_csv)
+        
+        expect { uploader.process }.to raise_error(
+          ServiceUploader::ServiceUploadError,
+          'Upload unsuccessful. Service Website google.com is not a valid URL for location id: 1'
+        )
+      end
 
       it 'should update a service if it exists already' do
         uploader = ServiceUploader.new(good_file_path)
@@ -64,16 +85,6 @@ describe ServiceUploader do
         services = uploader.process
         service = services.first
 
-        # testing = Rails.root.join("spec", "support", "fixtures", "service_uploader_test.csv")
-        # uploader1 = ServiceUploader.new(testing)
-        # services1 = uploader.process
-        # service1 = services1.first
-
-        # # p services
-        # p service.contacts
-        # # p services1
-        # p service1.contacts
-
         expect(service.contacts.map(&:name)).to include("Jaime Tester")
         expect(service.contacts.map(&:title)).to include("Dev")
         expect(service.contacts.map(&:email)).to include("jaime@test.io")
@@ -83,13 +94,27 @@ describe ServiceUploader do
         uploader = ServiceUploader.new(good_file_path)
         services = uploader.process
         service = services.first
+        
         expect(service.contacts.map(&:name)).to include("Jaime Tester")
+        expect(service.contacts.map(&:title)).to include("Dev")
+        expect(service.contacts.map(&:email)).to include("jaime@test.io")
 
-        # service.contacts.update(description: 'Description of this service')
+        service.contacts.update(
+          name: 'Cameron Test', 
+          title: 'Director', 
+          email: 'cameron@test.io'
+        )
+        expect(service.contacts.map(&:name)).to include("Cameron Test")
+        expect(service.contacts.map(&:title)).to include("Director")
+        expect(service.contacts.map(&:email)).to include("cameron@test.io")
 
-        # uploader = ServiceUploader.new(good_file_path)
-        # uploader.process
-        # expect(service.reload.description).to eq 'Uploaded service'
+        uploader = ServiceUploader.new(good_file_path)
+        uploader.process
+        expect(service.reload.description).to eq 'Uploaded service'
+
+        expect(service.contacts.map(&:name)).to include("Jaime Tester")
+        expect(service.contacts.map(&:title)).to include("Dev")
+        expect(service.contacts.map(&:email)).to include("jaime@test.io")
       end
   end
 end
