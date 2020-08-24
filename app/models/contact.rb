81ab195b5398948d126089833afcb0fceecf4d15
Stephanie Vizzi <stephanie@smartlogic.io>
2020-08-24 12:08:15 -0400

    fixes for test: spec/models/contact_spec.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,11 +2,11 @@ class Contact < ApplicationRecord
 
   default_scope { order('id ASC') }
 
-  belongs_to :location, touch: true
-  belongs_to :organization, touch: true
-  belongs_to :service, touch: true
-
   has_many :resource_contacts, dependent: :destroy
+  has_many :organizations, through: :resource_contacts, source: :resource, source_type: Organization.to_s
+  has_many :locations, through: :resource_contacts, source: :resource, source_type: Location.to_s
+  has_many :services, through: :resource_contacts, source: :resource, source_type: Service.to_s
+
   has_many :phones, dependent: :destroy, inverse_of: :contact
   accepts_nested_attributes_for :phones,
                                 allow_destroy: true, reject_if: :all_blank
