2d6898b5504a2db12261881d13989425e0dd05d2
Justus Eapen <justuseapen@Alyosha.local>
2020-11-03 13:17:45 -0500

    Fixes get analytics response handling

--- app/controllers/admin/analytics_controller.rb
+++ app/controllers/admin/analytics_controller.rb
@@ -8,9 +8,9 @@ class Admin
     layout 'admin'
 
     def index
-      @ui_analytics = get_ui_analytics
-      @total_homepage_views = total_homepage_views(@ui_analytics)
-      @new_homepage_views = new_homepage_views(@ui_analytics, @start_date, @end_date)
+      @total_homepage_views = get_ui_analytics['total_homepage_views']
+      @new_homepage_views = get_ui_analytics['new_homepage_views']
+
       @organizations_count = Organization.all.count
       @new_org_count = Organization.where(created_at: @start_date.beginning_of_day..@end_date.end_of_day).count
       @locations_count = Location.all.count
--- app/controllers/concerns/analytics.rb
+++ app/controllers/concerns/analytics.rb
@@ -2,23 +2,9 @@ require 'open-uri'
 
 module Analytics
   def get_ui_analytics
-    ui_analytics_api_url = ENV['UI_URL'] + '/api/analytics'
+    ui_analytics_api_url = ENV['UI_URL'] + "/api/analytics?start_date=#{@start_date.to_s}&end_date=#{@end_date}"
+    
     analytics_json = open(ui_analytics_api_url).read
     JSON.parse(analytics_json)
-  end
-
-  def total_homepage_views(analytics)
-    analytics.count do |obj|
-      landing_page = obj['landing_page']
-      landing_page == ENV['UI_HOMEPAGE_URL']
-    end
-  end
-
-  def new_homepage_views(analytics, start_date, end_date)
-    analytics.count do |obj|
-      created_at = Date.parse(obj['started_at'])
-      landing_page = obj['landing_page']
-      landing_page == ENV['UI_HOMEPAGE_URL'] && created_at.between?(start_date, end_date)
-    end
   end
 end
