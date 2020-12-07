3b8277736b81b3bf427d8628e6617669b0199957
Anna Dorigo <alejandr@gmail.com>
2020-12-07 14:31:15 -0500

    add keyword searching on location services tags

--- app/chewy/locations_index.rb
+++ app/chewy/locations_index.rb
@@ -21,6 +21,7 @@ class LocationsIndex < Chewy::Index
     field :name, analyzer: 'remove_stop_words'
     field :organization_id, type: 'integer'
     field :organization_name, value: -> { organization.try(:name) }, analyzer: 'remove_stop_words'
+    field :organization_tags, value: -> { organization.tags.map(&:name) }
     field :updated_at, type: 'date'
     field :zipcode, value: -> { address.try(:postal_code) }
     field :category_ids, value: -> { services.map(&:categories).flatten.uniq.map(&:id) }
@@ -28,7 +29,7 @@ class LocationsIndex < Chewy::Index
     field :tags, value: -> { tags.map(&:name) }
     field :featured_at, type: 'date'
     field :covid19, value: -> { covid19? ? created_at : nil }, type: 'date'
-    field :organization_tags, value: -> { organization.tags.map(&:name) }
+    field :service_tags, value: -> { services.map(&:tags).flatten.uniq.map(&:name) }
   end
 end
 
