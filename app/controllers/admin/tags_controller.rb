39c32b6657196f9fe487f2c947af552007e945cc
Anna Dorigo <alejandr@gmail.com>
2021-01-21 13:13:36 -0500

    add create functionality

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -9,5 +9,24 @@ class Admin
       @tags = Tag.all
     end
 
+    def new
+      @tag = Tag.new
+    end
+
+    def create
+      #prepare_and_authorize_service_creation
+
+      permitted_params = params.require(:tag).permit(:name)
+      
+      @tag = Tag.new(name: permitted_params["name"])
+      
+      if @tag.save
+        redirect_to admin_tags_path,
+                    notice: "Tag '#{@tag.name}' was successfully created."
+      else
+        render :new
+      end
+    end
+
   end
 end
