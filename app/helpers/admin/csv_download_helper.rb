c2e3ed7b5d6ee0e56a30c2b09542d5b83a92b82d
Justus Eapen <justuseapen@gmail.com>
2021-02-01 21:15:16 -0500

    Adds service categories report

--- app/helpers/admin/csv_download_helper.rb
+++ app/helpers/admin/csv_download_helper.rb
@@ -2,7 +2,7 @@ class Admin
   module CsvDownloadHelper
     def csv_tables
       %w[addresses categories contacts covid_19_locations food_and_covid_services holiday_schedules locations
-         organizations phones regular_schedules services]
+         organizations phones regular_schedules services service_categories]
     end
   end
 end
