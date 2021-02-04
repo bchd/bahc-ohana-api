72af8ce7d1a018eef147c0fb6258cc662e3501ba
Anna Dorigo <alejandr@gmail.com>
2021-02-04 12:59:27 -0500

    add update_index function missing

--- app/models/tag_resource.rb
+++ app/models/tag_resource.rb
@@ -1,4 +1,16 @@
 class TagResource < ApplicationRecord
+  # We want to skip organization because the index does not exist
+  # Keep this up to date with any taggable resource
+  update_index(-> (tag_resource) {
+    case tag_resource.resource_type 
+    when "Location" then "locations#location"
+    when "Service" then "services#service"
+    end
+  }) do
+    return nil if self.resource_type == "Organization"
+    self.resource
+  end
+
   belongs_to :tag
   belongs_to :resource, polymorphic: true
 
