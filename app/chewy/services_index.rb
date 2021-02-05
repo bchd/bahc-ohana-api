32cca9966062cfb0a6b2809f82d9434e13c89414
Anna Dorigo <alejandr@gmail.com>
2021-02-05 11:40:25 -0500

    add touch true in order to activate update index at the resource model

--- app/chewy/services_index.rb
+++ app/chewy/services_index.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ServicesIndex < Chewy::Index
-  define_type Service.includes(:location) do
+  define_type Service.includes(:location, :tags) do
     field :archived_at, type: 'date'
     field :archived, type: 'boolean', value: -> { !archived_at.nil? } 
     field :id, type: 'integer'
--- app/models/tag_resource.rb
+++ app/models/tag_resource.rb
@@ -1,18 +1,9 @@
 class TagResource < ApplicationRecord
   # We want to skip organization because the index does not exist
   # Keep this up to date with any taggable resource
-  update_index(-> (tag_resource) {
-    case tag_resource.resource_type 
-    when "Location" then "locations#location"
-    when "Service" then "services#service"
-    end
-  }) do
-    return nil if self.resource_type == "Organization"
-    self.resource
-  end
-
+  
   belongs_to :tag
-  belongs_to :resource, polymorphic: true
+  belongs_to :resource, polymorphic: true, touch: true
 
   def self.get_resources (tag_id)
     where("tag_id = ?", tag_id).select(:resource_id, :resource_type)
