a25600ac1dbbf1f2cb3fecf1f92b0293c5f21a85
Justus Eapen <justuseapen@gmail.com>
2021-03-16 17:02:17 -0400

    Adds spec for service categories report

---
+++ spec/features/admin/csv/download_service_categories_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+feature 'Downloading Service Categories CSV' do
+  before { login_super_admin }
+
+  context 'services has non-empty array attributes' do
+    before do
+      @food = create(:category)
+      @health = create(:health)
+      @jobs = create(:jobs)
+      @service = create(
+        :service,
+        category_ids: [@food.id, @health.id, @jobs.id]
+      )
+
+      @archived_service = create(
+        :service,
+        archived_at: DateTime.now
+      )
+
+      @unarchived_service_at_an_archived_location = create(
+        :service,
+        location: create(:location, archived_at: DateTime.now)
+      )
+
+      visit admin_csv_service_categories_path(format: 'csv')
+    end
+
+    it 'contains the same headers as in the import Wiki' do
+      expect(csv.first).to eq %w[
+      current_parent_categories name id location_name new_category new_subcategory clear_categories
+      ]
+    end
+
+    it 'does not contain archived services' do
+      expect(csv.second).to eq([
+        @service.current_parent_categories, @service.name, @service.id.to_s, @service.location_name, "", "", ""
+      ])
+        
+    end
+
+    it 'does not contain unarchived services that belong to archived locations' do
+      expect(csv.length).to eq(2)
+    end
+
+    it 'contains unarchived services that belong to unarchived locations' do
+      expect(csv.length).to eq(2)
+    end
+  end
+end
