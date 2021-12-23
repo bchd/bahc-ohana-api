e2520b3bf55458d62688ed9d7f5b935a63b80b65
Stone Filipczak <stone@smartlogic.io>
2021-12-23 11:40:53 -0500

    add label to holiday schedules

--- db/structure.sql
+++ db/structure.sql
@@ -393,7 +393,8 @@ CREATE TABLE public.holiday_schedules (
     start_date date NOT NULL,
     end_date date NOT NULL,
     opens_at time without time zone,
-    closes_at time without time zone
+    closes_at time without time zone,
+    label character varying
 );
 
 
@@ -1718,6 +1719,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20200810190344'),
 ('20210203011216'),
 ('20210705015154'),
-('20211108235002');
+('20211108235002'),
+('20211222211416');
 
 
