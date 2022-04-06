5362bcf71921ed659cd83ac9a0714c300325492e
rachel kahtan <rachel@smartlogic.io>
2022-04-06 12:10:54 -0400

    Move app-init line to bottom of application.js so it doesn't affect following code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import 'uppy/dist/uppy.min.css'
 import '../add_jquery'
 
 import { multipleFileUpload, singleFileUpload } from '../fileUpload'
-// require('../app/app-init');
+
 
 // Use 'DOMContentLoaded' event if not using Turbolinks (now Turbo)
 document.addEventListener('turbo:load', () => {
@@ -19,3 +19,5 @@ document.addEventListener('turbo:load', () => {
     }
   })
 })
+
+require('../app/app-init');
