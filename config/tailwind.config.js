5201f5cd01616c9ddbe34b0c4c7a79cb37039758
Stone Filipczak <stone@smartlogic.io>
2022-11-16 10:25:27 -0500

    use Lato as default font for admin

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,7 +12,10 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Lato', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'bchd-teal': '#007FAA',
       },
     },
   },
