a455e90593dea50ad8277d5ef0048d60b15f4156
Dan Ivovich <divovich@gmail.com>
2023-08-10 10:00:33 -0400

    Separate out API key for server side vs UI

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,7 +1,7 @@
 Geocoder.configure(
   lookup: :google,
   http_proxy: ENV['QUOTAGUARD_URL'],
-  api_key: ENV['GOOGLE_MAPS_API_KEY'],
+  api_key: ENV['GOOGLE_MAPS_SERVER_API_KEY'],
   always_raise: [
     Geocoder::OverQueryLimitError,
     Geocoder::RequestDenied,
