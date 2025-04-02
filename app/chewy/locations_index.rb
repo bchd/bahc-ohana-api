8520a7e036cb45da531855c8b89a7ded9cf9f5a6
Dan Ivovich <divovich@gmail.com>
2025-04-02 14:14:25 -0400

    Ignore archived services

--- app/chewy/locations_index.rb
+++ app/chewy/locations_index.rb
@@ -22,9 +22,9 @@ class LocationsIndex < Chewy::Index
   field :accessibility
   field :archived, type: 'boolean', value: -> { !archived_at.nil? } 
   field :archived_at, value: -> { archived_at? ? archived_at : nil }, type: 'date'
-  field :categories, value: -> { services.map(&:categories).flatten.uniq.map(&:name) }
-  field :categories_exact, analyzer: 'case_insensitive', value: -> { services.flat_map(&:categories).select { |cat| cat.ancestry.blank? }.uniq.map(&:name) }
-  field :category_ids, value: -> { services.map(&:categories).flatten.uniq.map(&:id) }
+  field :categories, value: -> { services.unarchived.map(&:categories).flatten.uniq.map(&:name) }
+  field :categories_exact, analyzer: 'case_insensitive', value: -> { services.unarchived.flat_map(&:categories).select { |cat| cat.ancestry.blank? }.uniq.map(&:name) }
+  field :category_ids, value: -> { services.unarchived.map(&:categories).flatten.uniq.map(&:id) }
   field :coordinates, type: 'geo_point', value: -> do
     if latitude? && longitude?
       {lat: latitude, lon: longitude}
@@ -37,20 +37,20 @@ class LocationsIndex < Chewy::Index
   field :description, analyzer: 'remove_stop_words'
   field :featured_at, type: 'date'
   field :id, type: 'integer'
-  field :keywords, value: -> { services.map(&:keywords).compact.join(', ') }, analyzer: 'remove_stop_words'
+  field :keywords, value: -> { services.unarchived.map(&:keywords).compact.join(', ') }, analyzer: 'remove_stop_words'
   field :name, type: 'text', analyzer: 'remove_stop_words'
   field :name_exact, analyzer: 'case_insensitive', value: -> { name }
   field :organization_id, type: 'integer'
   field :organization_name, value: -> { organization.try(:name) }, analyzer: 'remove_stop_words'
   field :organization_name_exact, analyzer: 'case_insensitive', value: -> { organization.try(:name) }
   field :organization_tags, value: -> { organization.tags.pluck(:name) }
-  field :service_descriptions, value: ->{ services.map(&:description) }
-  field :service_names, value: ->{ services.map(&:name) }
-  field :service_tags, value: -> { services.map(&:tags).flatten.uniq.map(&:name) }
-  field :sub_categories_exact, analyzer: 'case_insensitive', value: -> { services.flat_map(&:categories).select { |cat| !cat.ancestry.blank? }.uniq.map(&:name) }
+  field :service_descriptions, value: ->{ services.unarchived.map(&:description) }
+  field :service_names, value: ->{ services.unarchived.map(&:name) }
+  field :service_tags, value: -> { services.unarchived.map(&:tags).flatten.uniq.map(&:name) }
+  field :sub_categories_exact, analyzer: 'case_insensitive', value: -> { services.unarchived.flat_map(&:categories).select { |cat| !cat.ancestry.blank? }.uniq.map(&:name) }
   field :tags, value: -> { tags.map(&:name) }
   field :updated_at, type: 'date'
   field :zipcode, value: -> { address.try(:postal_code) }
-  field :languages, type: 'keyword', value: -> { services.map(&:languages).concat(languages).flatten.uniq.reject(&:blank?) }
+  field :languages, type: 'keyword', value: -> { services.unarchived.map(&:languages).concat(languages).flatten.uniq.reject(&:blank?) }
   field :open_flags, type: 'integer', value: -> {Flag.where(resource_id: id, completed_at: nil, resource_type: "Location").count}
 end
