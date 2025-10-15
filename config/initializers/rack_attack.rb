ed1a8c76ffde5c3029adb5560b157021a04ca0b8
Dan Ivovich <divovich@gmail.com>
2025-10-14 22:10:26 -0400

    Update filtering

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,9 +1,20 @@
 if ENV['ENABLE_RACK_ATTACK'] == 'true' && ENV['REDIS_URL'].present?
   Rack::Attack.cache.store = ActiveSupport::Cache::RedisCacheStore.new(url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
-  limit = ENV.fetch('IP_LIMIT', 15).to_i
-  period = ENV.fetch('IP_PERIOD', 60).to_i.seconds
+
+  # Focus on the /location paths
+  limit = ENV.fetch('IP_LIMIT', 30).to_i
+  period = ENV.fetch('IP_PERIOD', 120).to_i.seconds
   Rack::Attack.throttle('req/ip', limit: limit, period: period) do |request|
-    request.ip
+    if /\/location/ =~ request.path
+      request.ip
+    end
+  end
+
+  # Be really harsh on emoji in keyword params, which is the biggest source of issues
+  Rack::Attack.throttle('req/keyword-emoji/ip', limit: 10, period: 5.minutes) do |request|
+    if request.params['keyword'].present? && /\p{Emoji}/ =~ request.params['keyword']
+      request.ip
+    end
   end
 else
   Rack::Attack.enabled = false
