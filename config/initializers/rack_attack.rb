cf5b8506bb7d80b240cd35aa22396db7f4369ff4
Dan Ivovich <divovich@gmail.com>
2025-10-14 22:48:54 -0400

    More aggressive fail2ban style blocking

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -10,10 +10,21 @@ if ENV['ENABLE_RACK_ATTACK'] == 'true' && ENV['REDIS_URL'].present?
     end
   end
 
-  # Be really harsh on emoji in keyword params, which is the biggest source of issues
-  Rack::Attack.throttle('req/keyword-emoji/ip', limit: 10, period: 5.minutes) do |request|
-    if request.params['keyword'].present? && /\p{Emoji}/ =~ request.params['keyword']
-      request.ip
+  # Be really harsh on emoji in keyword params, which is big source of issues
+  Rack::Attack.blocklist('allow2ban-keyword-emoji') do |request|
+    Rack::Attack::Allow2Ban.filter(request.ip, maxretry: 10, findtime: 2.minutes, bantime: 1.hour) do
+      if request.params['keyword'].present? && /\p{Emoji}/ =~ request.params['keyword']
+        request.ip
+      end
+    end
+  end
+
+  # Be harsh on long spammy keyword params, which is a big source of issues
+  Rack::Attack.blocklist('allow2ban-keyword-length') do |request|
+    Rack::Attack::Allow2Ban.filter(request.ip, maxretry: 10, findtime: 1.minute, bantime: 2.minutes) do
+      if request.params['keyword'].to_s.length > 60
+        request.ip
+      end
     end
   end
 else
