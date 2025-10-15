a63d2a4cf5e8a5e9387a96b76b29248f44d37be7
Dan Ivovich <divovich@gmail.com>
2025-10-14 21:45:16 -0400

    Fix rack attack setup and redis connection

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,8 @@
 if ENV['ENABLE_RACK_ATTACK'] == 'true' && ENV['REDIS_URL'].present?
-  Rack::Attack.cache.store = ActiveSupport::Cache::RedisCacheStore.new(url: ENV['REDIS_URL'])
-  Rack::Attack.throttle("requests by ip", limit: 6, period: 30) do |request|
+  Rack::Attack.cache.store = ActiveSupport::Cache::RedisCacheStore.new(url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+  limit = ENV.fetch('IP_LIMIT', 15).to_i
+  period = ENV.fetch('IP_PERIOD', 60).to_i.seconds
+  Rack::Attack.throttle('req/ip', limit: limit, period: period) do |request|
     request.ip
   end
 else
