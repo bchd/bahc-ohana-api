ea36254563db740ac3d6226a8d01f7587833023e
Stone Filipczak <stone@smartlogic.io>
2026-04-02 15:02:21 -0400

    update test

--- spec/features/admin/categories/delete_subcategory_spec.rb
+++ spec/features/admin/categories/delete_subcategory_spec.rb
@@ -9,11 +9,9 @@ feature 'Delete Subcategory' do
   end
 
   scenario 'delete subcategory', js: true do
-    expect(page).to have_content @jobs_subcategory.name
     accept_confirm do
         click_link(href: "/admin/categories/#{@jobs_subcategory.id}")
     end
     expect(page).to have_content "Category '#{@jobs_subcategory.name}' was successfully deleted."
-    expect(page).not_to have_content @jobs_subcategory.name
   end
 end
