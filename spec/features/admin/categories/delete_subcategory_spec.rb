f5f71572ae6c850692889807fb55366b2713bf29
Stone Filipczak <stone@smartlogic.io>
2026-04-07 14:41:53 -0400

    remove ujs

--- spec/features/admin/categories/delete_subcategory_spec.rb
+++ spec/features/admin/categories/delete_subcategory_spec.rb
@@ -16,11 +16,6 @@ feature 'Delete Subcategory' do
       find_by_id("category-#{@jobs_subcategory.id}-delete").click
     end
 
-    # This extra accept_confirm is required because of a bug
-    # Confirm dialog appears twice throughout the app 
-    # This is a known issue and will be fixed in the future
-    accept_confirm
-
     expect(page).to have_content "Category '#{@jobs_subcategory.name}' was successfully deleted."
   end
 end
