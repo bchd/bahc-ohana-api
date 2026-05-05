9db034122cfc3002388b9acc23c3abd53ad8370c
Stone Filipczak <stone@smartlogic.io>
2026-05-04 20:03:02 -0400

    update app-wide references

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,12 +11,15 @@
 // about supported directives.
 //
 //= require jquery
-//= require turbolinks
 //= require bootstrap-sprockets
 
-document.addEventListener("turbolinks:before-cache", function() {
+document.addEventListener("turbo:before-cache", function() {
   const flash_message_element = document.querySelector(".alert")
   if (flash_message_element) {
     flash_message_element.remove()
   }
+})
+
+$(document).on("turbo:load", () => {
+  console.log("ON TURBOOOO!")
 })
