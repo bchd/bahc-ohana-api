f1e9001c23652e544bba1c4a1aaa31b2e0897356
Stone Filipczak <stone@smartlogic.io>
2026-05-22 11:22:40 -0400

    specify enabled environments in config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,5 +2,6 @@ if ENV['SENTRY_DSN'].present?
   Sentry.init do |config|
     config.dsn = ENV['SENTRY_DSN']
     config.environment = ENV['SENTRY_ENV'] || Rails.env
+    config.enabled_environments = %w[production staging dev]
   end
 end
